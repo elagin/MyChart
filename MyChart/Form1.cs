3f799ff11111a2350cc87dcdb3c4d5f38e1e3966
Elagin Pavel <elagin.pasha@gmail.com>
2018-04-04 22:54:35 +0300

    Go!

---
+++ MyChart/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyChart
+{
+    public partial class Form1 : Form
+    {
+        //todo: https://habrahabr.ru/post/204308/
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
