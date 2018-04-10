8b548271cddb40f2567b9927f25f0c86a46f552f
Elagin Pavel <elagin.pasha@gmail.com>
2018-04-10 23:11:17 +0300

    Калибровка акселерометра

--- MyChart/Form1.Designer.cs
+++ MyChart/Form1.Designer.cs
@@ -34,7 +34,12 @@ namespace MyChart
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboPorts = new System.Windows.Forms.ComboBox();
+            this.buttonRescan = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,30 +47,82 @@ namespace MyChart
             // 
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
+            //series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
             series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            //series1.ChartType = SeriesChartType.Line;
-            series1.ChartType = SeriesChartType.SplineArea;
+            series1.Name = "Name X";
 
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
             series2.ChartArea = "ChartArea1";
             series2.Legend = "Legend1";
-            series2.Name = "Series2";
+            series2.Name = "Name Y";
+
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Name Z";
             this.chart1.Series.Add(series1);
             this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(610, 319);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(813, 524);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
+            // 
+            // comboPorts
+            // 
+            this.comboPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboPorts.FormattingEnabled = true;
+            this.comboPorts.Location = new System.Drawing.Point(12, 12);
+            this.comboPorts.Name = "comboPorts";
+            this.comboPorts.Size = new System.Drawing.Size(121, 21);
+            this.comboPorts.TabIndex = 4;
+            // 
+            // buttonRescan
+            // 
+            this.buttonRescan.Location = new System.Drawing.Point(139, 10);
+            this.buttonRescan.Name = "buttonRescan";
+            this.buttonRescan.Size = new System.Drawing.Size(75, 23);
+            this.buttonRescan.TabIndex = 5;
+            this.buttonRescan.Text = "Обновить";
+            this.buttonRescan.UseVisualStyleBackColor = true;
+            this.buttonRescan.Click += new System.EventHandler(this.buttonRescan_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 39);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 6;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(93, 39);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 7;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 343);
+            this.ClientSize = new System.Drawing.Size(813, 524);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonRescan);
+            this.Controls.Add(this.comboPorts);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -77,6 +134,10 @@ namespace MyChart
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboPorts;
+        private System.Windows.Forms.Button buttonRescan;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
     }
 }
 
--- MyChart/Form1.cs
+++ MyChart/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.OleDb;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -12,11 +13,25 @@ namespace MyChart
 {
     public partial class Form1 : Form
     {
+        SerialPort serialPort;
+        readonly string DATE_FORMAT = "dd.MM.yyyy HH:mm:ss";
+        private int iter = 0;
+
         //todo: https://habrahabr.ru/post/204308/
         public Form1()
         {
             InitializeComponent();
-            draw();
+            this.FormClosed += new FormClosedEventHandler(this.MainForm_FormClosed);
+            /*
+                        int xCalibration = -28081;
+                        int yCalibration = -23732;
+                        int zCalibration = -12798;
+
+                        int calibrationCount = 100;
+                        int dx = xCalibration / calibrationCount;
+                        int dy = yCalibration / calibrationCount;
+                        int dz = zCalibration / calibrationCount;*/
+            scanPorts();
         }
 
         private void loadData()
@@ -46,8 +61,6 @@ namespace MyChart
             myConnection.Close();
         }
 
-
-
         private void draw()
         {
             // Set primary x-axis properties
@@ -60,7 +73,7 @@ namespace MyChart
             chart1.ChartAreas["ChartArea1"].AxisY.MinorGrid.Interval = 0.25;
             chart1.ChartAreas["ChartArea1"].AxisY.MajorGrid.Interval = 0.5;
             chart1.ChartAreas["ChartArea1"].AxisY.LabelStyle.Interval = 0.5;
-
+            /*
             // Add data points to the series that have the specified X and Y values
             for (double t = 0; t <= (2.5 * Math.PI); t += Math.PI / 6)
             {
@@ -69,6 +82,153 @@ namespace MyChart
                 chart1.Series["Series1"].Points.AddXY(t, ch1);
                 chart1.Series["Series2"].Points.AddXY(t, ch2);
             }
+            */
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String comName = comboPorts.SelectedItem.ToString();
+                serialPort = new SerialPort();
+                serialPort.PortName = comName;
+                serialPort.BaudRate = 9600;
+                serialPort.ReadTimeout = 500;
+                serialPort.WriteTimeout = 500;
+                serialPort.Open();
+                serialPort.DataReceived += serialPort2_DataReceived;
+                buttonStart.Enabled = false;
+                buttonStop.Enabled = true;
+                buttonRescan.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                serialPort = null;
+                string caption = "Ошибка.";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+                result = MessageBox.Show(ex.Message, caption, buttons, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            closePort();
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+            buttonRescan.Enabled = true;
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            closePort();
+        }
+
+        private void closePort()
+        {
+            if (serialPort != null)
+            {
+                serialPort.Close();
+                serialPort = null;
+            }
+        }
+
+        private void serialPort2_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string pot = serialPort.ReadLine();
+            BeginInvoke(new LineReceivedEvent(LineReceived), pot);
+        }
+
+        private delegate void LineReceivedEvent(string POT);
+
+        int currentCount = 0;
+        int calibrationCount = 100;
+        int xCalibration = 0;
+        int yCalibration = 0;
+        int zCalibration = 0;
+
+        private void LineReceived(string line)
+        {
+            string[] values = line.Remove(line.Length - 1, 1).Split(':');
+            int x;
+            int y;
+            int z;
+            if (!Int32.TryParse(values[1], out x))
+                Console.WriteLine("String could not be parsed.");
+            if (!Int32.TryParse(values[2], out y))
+                Console.WriteLine("String could not be parsed.");
+            if (!Int32.TryParse(values[3], out z))
+                Console.WriteLine("String could not be parsed.");
+
+            if (currentCount < calibrationCount)
+            {
+                xCalibration = xCalibration + x;
+                yCalibration = yCalibration + y;
+                zCalibration = zCalibration + z;
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"WriteLines.csv", true))
+                {
+                    /*
+                    StringBuilder save = new StringBuilder();
+                    save.Append(x);
+                    save.Append(";");
+                    save.Append(y);
+                    save.Append(";");
+                    save.Append(z);
+                    save.Append(";");
+                    save.Append(xCalibration);
+                    save.Append(";");
+                    save.Append(yCalibration);
+                    save.Append(";");
+                    save.Append(zCalibration);
+                    file.WriteLine(save.ToString());
+                    */
+                    currentCount++;
+                    if (currentCount == calibrationCount)
+                    {
+                        xCalibration = xCalibration / calibrationCount;
+                        yCalibration = yCalibration / calibrationCount;
+                        zCalibration = zCalibration / calibrationCount;
+/*
+                        file.WriteLine("===============");
+                        StringBuilder save2 = new StringBuilder();
+                        save2.Append(xCalibration);
+                        save2.Append(";");
+                        save2.Append(yCalibration);
+                        save2.Append(";");
+                        save2.Append(zCalibration);
+                        file.WriteLine(save2.ToString());
+                        file.WriteLine("===============");
+*/
+                    }
+                }
+            }
+            else
+            {
+                chart1.Series["Name X"].Points.AddXY(iter, x - xCalibration);
+                chart1.Series["Name Y"].Points.AddXY(iter, y - yCalibration);
+                chart1.Series["Name Z"].Points.AddXY(iter, z - zCalibration);
+                iter++;
+            }
+        }
+
+        private void buttonRescan_Click(object sender, EventArgs e)
+        {
+            scanPorts();
+        }
+
+        private void scanPorts()
+        {
+            comboPorts.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            for (int i = 0; i < ports.Length; i++)
+            {
+                comboPorts.Items.Add(ports[i]);
+            }
+            if (ports.Length > 0)
+                comboPorts.SelectedIndex = 0;
+            if (ports.Length == 1 || ports.Length == 0)
+                comboPorts.Enabled = false;
         }
     }
 }
+
